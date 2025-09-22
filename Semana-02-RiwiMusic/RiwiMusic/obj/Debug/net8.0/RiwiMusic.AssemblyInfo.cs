b80fe61b6394ea575b06ea4572bc583c473ccd7a
Farg0n <miguelanga1604@gmail.com>
2025-09-21 23:18:47 -0500

    feat: update all config files

--- Semana-02-RiwiMusic/RiwiMusic/obj/Debug/net8.0/RiwiMusic.AssemblyInfo.cs
+++ Semana-02-RiwiMusic/RiwiMusic/obj/Debug/net8.0/RiwiMusic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiwiMusic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f5f058da50dea7a85df0dba8b0eded00a19d78e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiwiMusic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiwiMusic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
