663d98dfd975254ebad06bdf5c86b2683dfe3cc6
Farg0n <miguelanga1604@gmail.com>
2025-10-05 09:11:01 -0500

    feat: correct heritance

--- Semana-04-DigitalLibrary/DigitalLibrary/Models/User.cs
+++ Semana-04-DigitalLibrary/DigitalLibrary/Models/User.cs
@@ -1,6 +1,11 @@
 namespace DigitalLibrary.Models;
 
-public class User
+public class User : Person
 {
-    
+    public DateOnly RegistrationDate;
+    public User(int id, string name, string docNumber, string email, int celNumber, DateOnly registrationDate) 
+        : base (id,  name,  docNumber, email, celNumber)
+    {
+        RegistrationDate = registrationDate;
+    }
 }
