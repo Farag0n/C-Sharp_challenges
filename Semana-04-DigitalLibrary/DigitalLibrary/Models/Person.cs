e62d295815d1f25ededa41872c30807b068e0773
Farg0n <miguelanga1604@gmail.com>
2025-10-05 18:01:17 -0500

    feat: age

--- Semana-04-DigitalLibrary/DigitalLibrary/Models/Person.cs
+++ Semana-04-DigitalLibrary/DigitalLibrary/Models/Person.cs
@@ -4,14 +4,16 @@ public abstract class Person
 {
     protected int ID { get; set; }
     protected string Name { get; set; }
+    protected int Age { get; set; }
     protected string DocNumber { get; set; }
     protected string Email { get; set; }
     protected int CelNumber { get; set; }
 
-    public Person(int id, string name, string docNumber, string email, int celNumber)
+    public Person(int id, string name, int age, string docNumber, string email, int celNumber)
     {
         ID = id;
         Name = name;
+        Age = age;
         DocNumber = docNumber;
         Email = email;
         CelNumber = celNumber;
--- Semana-04-DigitalLibrary/DigitalLibrary/Models/User.cs
+++ Semana-04-DigitalLibrary/DigitalLibrary/Models/User.cs
@@ -3,8 +3,8 @@ namespace DigitalLibrary.Models;
 public class User : Person
 {
     public DateOnly RegistrationDate;
-    public User(int id, string name, string docNumber, string email, int celNumber, DateOnly registrationDate) 
-        : base (id,  name,  docNumber, email, celNumber)
+    public User(int id, string name, int age, string docNumber, string email, int celNumber, DateOnly registrationDate) 
+        : base (id,  name, age,  docNumber, email, celNumber)
     {
         RegistrationDate = registrationDate;
     }
