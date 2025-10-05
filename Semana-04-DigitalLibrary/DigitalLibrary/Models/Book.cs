c55668711455f31c6ca6b16104c391e68ec1e424
Farg0n <miguelanga1604@gmail.com>
2025-10-05 09:11:40 -0500

    feat: attribues & ctor

--- Semana-04-DigitalLibrary/DigitalLibrary/Models/Book.cs
+++ Semana-04-DigitalLibrary/DigitalLibrary/Models/Book.cs
@@ -2,5 +2,18 @@ namespace DigitalLibrary.Models;
 
 public class Book
 {
-    
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string? Writer { get; set; }
+    public DateOnly Relased{ get; set; }
+    public int Stock { get; set; }
+
+    public Book(int id, string name, string? writer, DateOnly relased, int stock)
+    {
+        ID = id;
+        Name = name;
+        Writer = writer;
+        Relased = relased;
+        Stock = stock;
+    }
 }
