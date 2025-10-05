a56f2ad534ae8dc3caa4ea1c09e36215e4926474
Farg0n <miguelanga1604@gmail.com>
2025-10-05 09:57:05 -0500

    edit: remame folder

--- Semana-04-DigitalLibrary/DigitalLibrary/Models/LoanHistory.cs
+++ Semana-04-DigitalLibrary/DigitalLibrary/Models/LoanHistory.cs
@@ -2,14 +2,14 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace DigitalLibrary.Models;
 
-public class LendingHistory
+public class LoanHistory
 {
     public int ID { get; set; }
     public int UserId { get; set; }
     public int BookId { get; set; }
     public int BookLendingId { get; set; }
 
-    public LendingHistory(int id, int userId, int bookId, int bLendingId)
+    public LoanHistory(int id, int userId, int bookId, int bLendingId)
     {
         ID = id;
         UserId = userId;
